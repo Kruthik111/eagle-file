a13ca9ee4415844c88707321a209c3166aea1b74
Kruthik S <gokruthik2003@gmail.com>
2025-01-15 21:43:46 +0530

    improved project structure

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,10 @@
-console.log(first);
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+
+app.use();
+
+export { app };
