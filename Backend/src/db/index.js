6b9a618b26939d438dac79b26c4fb7ee8bce7141
Kruthik S <kruthik.s@storeking.in>
2025-09-02 10:08:31 +0530

    code cleanup

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../../src/constants.js";
 import "dotenv/config";
 
+let MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/files";
+
 async function connectDB() {
   try {
-    const connectionInstance = await mongoose.connect(
-      // `${process.env.MONGODB_URL}/${DB_NAME}`
-      `${process.env.MONGODB_URL}`
-    );
+    const connectionInstance = await mongoose.connect(MONGODB_URL);
     console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance}`);
   } catch (error) {
     console.error("Error occured while connecting to Database:", error);
